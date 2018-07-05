eb129109ff2c8894cfaccbcf07bc71cb34ecc3ca
Matthew Hummer <biggreyson@gmail.com>
2018-07-04 22:32:48 -0600

    add User#to_s

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :roles, foreign_key: :volunteer_id
   scope :lusers,     -> {where({:luser=>true})}
   scope :registered, -> {where({:luser=>false})}
+
+  def to_s
+    return [self.first_name, self.last_name].compact.join(' ')
+  end
 end
